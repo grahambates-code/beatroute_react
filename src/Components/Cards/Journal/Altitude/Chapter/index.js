8c17cd90805ed91e00b4d2d9c60efbe576047d1e
Lam Doan <thanhlam.fit@gmail.com>
2021-06-03 21:41:35 +0700

    add 3 descriptions

--- src/Components/Cards/Journal/Altitude/Chapter/index.js
+++ src/Components/Cards/Journal/Altitude/Chapter/index.js
@@ -1,11 +1,14 @@
-import React, {Component, useLayoutEffect, useRef, useState} from 'react';
+import React, { Component } from 'react';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
-import Deck from "../../../Common/Deck";
+import Deck from '../../../Common/Deck';
 
-export default class extends Component {
+import './index.less';
 
+gsap.registerPlugin(ScrollTrigger);
+
+export default class Chapter extends Component {
     constructor(props) {
         super(props);
 
@@ -14,28 +17,48 @@ export default class extends Component {
             selectedAsset: null,
             viewState: props.slide.camera
         }
-
     }
 
+    componentDidMount() {
+        //TODO: Handle GSAP here
+    }
+
+    setViewState = (p) => {
+        this.setState({viewState: p});
+    };
+
     render() {
-
         let { viewState } = this.state;
+        let { font, client, slide, refetch, width, trip, card, updateSlideCamera, gps_data } = this.props;
 
-        let {font, client, slide, refetch, width, trip, card, updateSlideCamera, gps_data} = this.props;
-
-        let setViewState = (p) => this.setState({viewState: p});
-
-        return <Deck slide={slide}
-                     client={client}
-                     refetch={refetch}
-                     updateSlideCamera={updateSlideCamera}
-                     font={font}
-                     viewState={viewState}
-                     setViewState={setViewState}
-                     width={width}
-                     trip={trip}
-                     gps_data={gps_data}
-                     card={card}/>
+        return (
+            <div className="chapter">
+                <Deck 
+                    slide={slide}
+                    client={client}
+                    refetch={refetch}
+                    updateSlideCamera={updateSlideCamera}
+                    font={font}
+                    viewState={viewState}
+                    setViewState={this.setViewState}
+                    width={width}
+                    trip={trip}
+                    gps_data={gps_data}
+                    card={card}
+                />
+                <div className="chapter-descriptions">
+                    <div className="chapter-description-wrapper">
+                        <p>Description 1</p>
+                    </div>
+                    <div className="chapter-description-wrapper">
+                        <p>Description 2</p>
+                    </div>
+                    <div className="chapter-description-wrapper">
+                        <p>Description 3</p>
+                    </div>
+                </div>
+            </div>
+        );
 
     }
 
